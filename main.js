ea95c69fb7b446937d2e8c7aed48bcfe3ab43634
myk_eshun <dev.mike.eshun@gmail.com>
2018-09-16 19:11:54 +0000

    used shorthand for v-bind

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ const submissionComponent = {
     `<div style="display: flex; width: 100%">
       <figure class="media-left">
         <img class="image is-64x64" 
-          v-bind:src="submission.submissionImage">
+          :src="submission.submissionImage">
       </figure>
 
       <div class="media-content">
         <div class="content">
           <p>
             <strong>
-              <a v-bind:href="submission.url" class="has-text-info">{{ submission.title }}</a>
+              <a :href="submission.url" class="has-text-info">{{ submission.title }}</a>
               <span class="tag is-small">#{{ submission.id }}</span>
             </strong>
             <br>
@@ -19,7 +19,7 @@ const submissionComponent = {
             <small class="is-size-7">
               Submitted by:
               <img class="image is-24x24" 
-                v-bind:src="submission.avatar">
+                :src="submission.avatar">
             </small>
           </p>
         </div>
